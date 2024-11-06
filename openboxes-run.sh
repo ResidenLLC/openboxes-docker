fd9849f134230a9ca45dc0f4d8066196e6933129
Assist CAJ <alexandru.duduman@assist.ro>
2024-11-06 17:19:33 +0000

    Adjustments:

--- openboxes-run.sh
+++ openboxes-run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# grails doesn't run without it for some reason
+# Grails does not run without these JVM options
 export GRAILS_OPTS="-XX:MaxPermSize=8192m -Xmx8192M -XX:-UseSplitVerifier -Xverify:none"
 
+# Force Grails upgrade if application context is missing
 if [ ! -f /app/web-app/WEB-INF/applicationContext.xml ]; then
    echo "Forcing grails upgrade"
    grails upgrade --force --stacktrace
 fi
 
-
+# Clear and verify npm cache
 npm cache clean --force
 npm cache verify
 npm i -g npm@6.14.6
 npm config set engine-strict true
-# npm update
-
-# some dependencies don't get resolved on thier own
 npm i --no-package-lock --legacy-peer-deps --force --verbose --stacktrace --unsafe-perm
-grails run-app --stacktrace
 
-# if you want to compile enable this and stop grails run-app
-# grails -Dgrails.env=staging war
+# Run the application in staging environment
+#grails run-app --stacktrace
+grails -Dgrails.env=production run-app --stacktrace
+
+
